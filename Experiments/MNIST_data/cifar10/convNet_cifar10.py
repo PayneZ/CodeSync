012e24e2da513b3fe4b97ba2836c641a9941065e
PayneZ <201421130016@mail.bnu.edu.cn>
2018-02-11 15:13:54 +0800

    added Experiments on tensorflow

---
+++ Experiments/MNIST_data/cifar10/convNet_cifar10.py
@@ -0,0 +1,25 @@
+import cifar10, cifar10_input
+import tensorflow as tf
+import numpy as np
+import time
+
+max_steps = 3000
+batch_size = 128
+data_dir = '/tmp/cifar10_data/cifar-10-batches-bin'
+
+def variable_with_weight_loss(shape, stddev, w1):
+    var = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+    if wl is not None:
+        weight_loss = tf.multiply(tf.nn.l2_loss(var), wl, name='weight_loss')
+        tf.add_to_collection('losses', weight_loss)
+    return var
+
+cifar10.maybe_download_and_extract()
+
+images_train, labels_train = cifar10_input.distored_inputs(
+    data_dir=data_dir, batch_size=batch_size)
+
+images_test, labels_test = cifar10_input.inputs(eval_data=True,
+                                                data_dir=data_dir,
+                                                batch_size=batch_size)
+
